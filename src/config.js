b3b5c0a4be742d502072df8e17de5a8782f2d6c6
Lokenwow <lokenwow@gmail.com>
2022-05-25 12:56:14 +0100

    Added config and constants files plus refactoring of folder structure.

---
+++ src/config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+global.config = {
+
+	user: {
+		username: 'lokenwow',
+	},
+
+	sign: {
+		signController: true,
+		signText: 'Feel free to check me out https://github.com/lokenwow',
+		resignCheckTimer: 500,
+	},
+
+	tower: {
+		healMyCreeps: true,
+		repairStructures: false
+	},
+
+	farmPixels: {
+		enabled: true,
+		minBucketAfter: 2500,
+	},
+	
+	allies: {
+		 
+	},
+
+	cpuStats: {
+		enabled: true,
+	},
+
+}

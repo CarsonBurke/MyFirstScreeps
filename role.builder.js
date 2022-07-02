b97590b676b19a436ed66362f4dca9ccb6e532a4
BlueSquidget <blue.tinee@gmail.com>
2022-07-02 10:31:11 +0100

    Changed memory.building in role.builder and memory.upgrading in role.upgrader to memory.working for easier future use. Added comments to role.builder and role.upgrader

--- role.builder.js
+++ role.builder.js
@@ -3,26 +3,40 @@ var roleBuilder = {
 	/** @param {Creep} creep **/
 	run: function (creep) {
 
-		if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-			creep.memory.building = false;
+		//if memory.working is true AND the creep has 0 energy in its store
+		if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
+			//set memory.working to false and get creep to say what it's doing
+			creep.memory.working = false;
 			creep.say('?? harvest');
 		}
-		if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-			creep.memory.building = true;
+
+		//if memory.working is false AND the creep's free capacity is 0
+		if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
+			//set memory.working to true and creep says what it's doing
+			creep.memory.working = true;
 			creep.say('?? build');
 		}
 
-		if (creep.memory.building) {
+		//if memory.working is true
+		if (creep.memory.working) {
+			//save array of construction sites to 'targets'
 			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			//if targets array is greater than 0, move/build
 			if (targets.length) {
+				//if first construction site in array is not in range to build
 				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+					//move to it
 					creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
 				}
 			}
 		}
+		//else if memory.working is false
 		else {
+			//save array of sources to 'sources'
 			var sources = creep.room.find(FIND_SOURCES);
+			//if first source in array is not in range to harvest
 			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+				//move to it
 				creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
 			}
 		}
--- role.upgrader.js
+++ role.upgrader.js
@@ -3,23 +3,35 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function (creep) {
 
-        if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
+        //if memory.working is true AND the creep has 0 energy in its store
+        if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
+            //set memory.working to false and get creep to say what it's doing
+            creep.memory.working = false;
             creep.say('?? harvest');
         }
-        if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
+
+        //if memory.working is false AND the creep's free capacity is 0
+        if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
+            //set memory.working to true and creep says what it's doing
+            creep.memory.working = true;
             creep.say('? upgrade');
         }
 
-        if (creep.memory.upgrading) {
+        //if memory.working is true
+        if (creep.memory.working) {
+            //if controller is not in range to upgrade
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                //move to it
                 creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
         }
+        //else if memory.working is false
         else {
+            //save array of sources to 'sources'
             var sources = creep.room.find(FIND_SOURCES);
+            //if first source in array is not in range to harvest
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                //move to it
                 creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }

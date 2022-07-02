03030aa6207c9b44745f5b302b44a72c3e1ea6ca
Lokenwow <lokenwow@gmail.com>
2022-07-02 13:28:41 +0100

    Broken build but added some notes and classes to help expplain

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var memorySetup = require('memorySetup');
 
 
 // Main Game loop
 module.exports.loop = function () {
 
+    if (!Memory.Setup) {
+        memorySetup.run();
+    }
+
+    const points = [];
+
+    for (let x = 1; x < 49; x++) {
+        for (let y = 1; y < 49; y++) {
+            //points.push(new RoomPosition(x,y,'sim'))
+            new RoomVisual('W1N1').rect(x - 0.5, y - 0.5, 1, 1, { fill: 'transparent', stroke: '#f00' });
+        }
+    }
+
     //Test visual
-    new RoomVisual().text("Home", 25, 25, { color: 'blue', font: 0.8 });
+    //new RoomVisual('sim').poly(points, { fill: 'aqua'});
+    //new RoomVisual('W1N1').rect(points, 9, 9, {fill: 'transparent', stroke: '#f00'});
+    //new RoomVisual('W1N1').rect(0, 0.5, 1, 1, {fill: 'transparent', stroke: '#f00'}); 
+
     
     //for every name in array of creeps
     for (var name in Game.creeps) {
@@ -29,5 +46,8 @@ module.exports.loop = function () {
             //Run builder logic
             roleBuilder.run(creep);
         }
+    }
+    if (memory.visuals) {
+        displayVisuals();
     }
 }
---
+++ src/memorySetup.js
@@ -0,0 +1,14 @@
+
+
+Memory.visuals = false;
+
+method: run(){
+
+	//this is where we set up all the memory stuff on a fresh install of the bot
+
+	//by default visuals off
+	memory.visuals = false;
+
+	//Example
+	//memory.maxHaulers = 5;
+}
